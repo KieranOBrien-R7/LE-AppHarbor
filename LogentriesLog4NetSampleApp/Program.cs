d3d056bbeeebd710bdacfd6730945ae3e40c7948
Kieran O'Brien <kieran.obrien@logentries.com>
2016-05-10 11:28:50 +0100

    AppHarbor test

---
+++ LogentriesLog4NetSampleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+namespace LogentriesLog4NetSampleApp
+{
+    class Program
+    {
+        private static readonly ILog loggerLe = LogManager.GetLogger( typeof(Program));
+        private static readonly ILog loggerFile = LogManager.GetLogger("File");
+        
+
+        static void Main(string[] args)
+        {
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+
+            String x = System.Console.ReadLine();
+        }
+    }
+}
